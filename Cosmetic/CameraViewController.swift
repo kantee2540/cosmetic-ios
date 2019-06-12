9570742a695bac5ef0618d3d73fc7ab1300f9479
kantee <kantee2540@hotmail.com>
2019-06-12 23:02:27 +0700

    Add Firebase and Camera

--- Cosmetic/CameraViewController.swift
+++ Cosmetic/CameraViewController.swift
@@ -7,22 +7,88 @@
 //
 
 import UIKit
-import SafariServices
+import Firebase
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    let vision = Vision.vision()
+    let cameraRunning = false
+    var captureSession: AVCaptureSession!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    var imageOutput: AVCapturePhotoOutput!
     
     @IBOutlet weak var cameraView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupCamera()
+        
+        let recognizer = vision.onDeviceTextRecognizer()
+        let image :UIImage = UIImage(named: "item2")!
+        let visionImage = VisionImage(image: image)
+        
+        recognizer.process(visionImage)  { result, error in
+            guard error == nil, let result = result else{
+                return
+            }
+            
+            print("Text that detected ===> " + result.text)
+        }
         
     }
     override func viewDidAppear(_ animated: Bool) {
         self.tabBarController?.navigationItem.title = "Search by Camera"
         
+        videoPreviewLayer!.frame = cameraView.bounds
+        
     }
     override func viewDidDisappear(_ animated: Bool) {
+        onStopCamera()
+    }
+    
+    func setupCamera(){
+        captureSession = AVCaptureSession()
+        //captureSession?.sessionPreset = .high
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else{
+                print("Unable to access back Camera!!")
+                return
+        }
+        
+        do{
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            imageOutput = AVCapturePhotoOutput()
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(imageOutput as AVCaptureOutput){
+                captureSession?.addInput(input)
+                captureSession.addOutput(imageOutput)
+                
+                setupLivePreview()
+                
+                onStartCamera()
+            }
+            
+            
+        }catch let error{
+            print("Error Camera!! ==> \(error)")
+        }
+        
+        
+        
+    }
+    func onStartCamera(){
+        captureSession.startRunning()
+    }
+    func onStopCamera(){
+        captureSession.stopRunning()
+    }
+    
+    
+    func setupLivePreview(){
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = .resizeAspect
+        cameraView.layer.addSublayer(videoPreviewLayer)
         
     }
     
